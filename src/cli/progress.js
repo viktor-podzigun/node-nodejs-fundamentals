a7f12b53032174cf6978a4715fba20ca3f802a4d
Viktor Podzigun <viktor.podzigun@gmail.com>
2026-03-19 14:28:06 +0100

    Implemented cli/progress

--- src/cli/progress.js
+++ src/cli/progress.js
@@ -3,6 +3,27 @@ const progress = () => {
   // Simulate progress bar from 0% to 100% over ~5 seconds
   // Update in place using \r every 100ms
   // Format: [████████████████████          ] 67%
+  const barChar = "█";
+  const barLength = "████████████████████          ".length;
+
+  let progress = 0;
+  const intervalId = setInterval(() => {
+    if (progress >= 1) {
+      process.stdout.write("\r");
+    }
+
+    const bar1 = barChar.repeat((barLength * progress) / 100);
+    const bar2 = " ".repeat(barLength - bar1.length);
+
+    process.stdout.write(`[${bar1}${bar2}] ${progress}%`);
+
+    if (progress >= 100) {
+      clearInterval(intervalId);
+      process.stdout.write("\n");
+    }
+
+    progress += 1;
+  }, 50);
 };
 
 progress();
--- test/all.js
+++ test/all.js
@@ -5,3 +5,4 @@ await import("../src/fs/findByExt.js");
 await import("../src/fs/merge.js");
 await import("../src/fs/snapshot.js");
 await import("../src/fs/restore.js");
+await import("../src/cli/progress.js");
